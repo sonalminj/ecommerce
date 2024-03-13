a4b59f8db36e5e31499cc0118fbd41ea16ed05c9
sonalminj <sonalminjprayas@gmail.com>
2024-03-13 19:50:27 +0530

    Add font awesome icon for shopping cart

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
-createApp(App).use(router).mount('#app');
+library.add(faShoppingCart)
+
+createApp(App).use(router).component("font-awesome-icon", FontAwesomeIcon).mount('#app');
